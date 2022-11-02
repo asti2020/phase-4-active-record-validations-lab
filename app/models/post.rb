0f823ff60bf48211846d094b3c48676299afbafb
Astika Luintel <astild2020@gmail.com>
2022-11-02 11:59:49 -0400

    commit tree c37c301769106b28a868454089e02b720a7caec2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, uniqueness: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    # def catagory_must_be_fiction_or_non_fiction
+    #     if !["Fiction", "Non-Fiction"].include?(category)
+    #         errors.add(:category, "must be Fiction or Non-Fiction")
+    #     end
+    # end
 end
